e676f0f6e897c5021e212f91b35c05aca62c0adc
Anna Pokidina <anna_pokidina@epam.com>
2016-10-05 12:48:05 +0300

    [npm install]test webpack build

--- src/components/components.js
+++ src/components/components.js
@@ -1,6 +1,5 @@
 import homeComponent from './home/home';
-import heroComponent from './hero/hero';
 
 export default app => {
-  INCLUDE_ALL_MODULES([homeComponent, heroComponent], app);
+  INCLUDE_ALL_MODULES([homeComponent], app);
 }
