a2e3c7a86d8a4328514763f6a8c45587fd5acd12
Viacheslav Dyomin <v.dyomin@gmail.com>
2016-10-07 01:30:16 +0300

    [webpack][config][npm install] separate style file in dist, all necessary file copy to dist folder, sourcemap for sas css

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var  nodeEnvironment = process.env.NODE_ENV;
 
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
 
     entry: {
@@ -35,9 +37,14 @@ module.exports = {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
             { test: /\.html/, exclude: /(node_modules)/, loader: 'html-loader' },
-            { test: /\.sass$/, loaders: [ 'style', 'css', 'resolve-url', 'sass' ] }
+            { test: /\.sass$/, loader: ExtractTextPlugin.extract('css?sourceMap!sass?sourceMap' )},
+            { test: /\.(jpg|png|svg|eot|otf|svg|ttf|woff|woff2)$/, loader:'file?name=[path][name].[ext]'}
 
-        ],
+        ]
 
-    }
+    },
+
+    plugins: [
+        new ExtractTextPlugin("main.css", {allChunks: true})
+    ]
 };
