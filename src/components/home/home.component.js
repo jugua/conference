f38cd78ce90d1be5fad91140141e7ee4528c8a2c
Anna Pokidina <anna_pokidina@epam.com>
2016-10-09 20:22:09 +0300

    [in process] change directive to component

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -3,15 +3,14 @@ import controller from './home.controller';
 
 // This is the Directive Definition Object function seen in a traditional Angular setup.
 // In this example it is abstracted as a shell and used in the home.js.
-const homeComponent = function home() {
-  return {
-    restrict: 'EA',
-    scope: {},
-    template: template,
-    controller: controller,
-    controllerAs: 'homeCtrl',
-    bindToController: true
-  };
-};
+const homeComponent = {
+  bindings: {
+    'name': '='
+  },
+
+  template: template,
+  controller: controller,
+  controllerAs: 'homeCtrl',
+  }
 
 export default homeComponent;
