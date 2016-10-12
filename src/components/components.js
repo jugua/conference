d4842edc8f061f3aedb0cd88b0451914b23b84b3
Ivan Tkachuk <ivan_tkachuk@epam.com>
2016-10-12 23:06:04 +0300

    [2263] import created component

--- src/components/components.js
+++ src/components/components.js
@@ -2,7 +2,8 @@
 import homeComponent from './home/home';
 import loginComponent from './login/login';
 import listComponent from './list/list';
+import singInComponent from './sing-in/sing-in';
 
 export default (app) => {
-  include_all_modules([homeComponent, loginComponent, listComponent], app);
+  include_all_modules([homeComponent, loginComponent, listComponent, singInComponent], app);
 };
