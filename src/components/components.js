2625a1005c4b55fc23b33d70cf7f618fab0994a0
Ivan Tkachuk <ivan_tkachuk@epam.com>
2016-10-13 14:14:20 +0300

    [NOJIRA] clear test folders. add fonts

--- src/components/components.js
+++ src/components/components.js
@@ -1,8 +1,6 @@
 /* global include_all_modules */
 import homeComponent from './home/home';
-import loginComponent from './login/login';
-import listComponent from './list/list';
 
 export default (app) => {
-  include_all_modules([homeComponent, loginComponent, listComponent], app);
+  include_all_modules([homeComponent], app);
 };
