32067136427c0035f68aa09730a457be0821cbaa
Viacheslav Dyomin <v.dyomin@gmail.com>
2016-10-14 20:33:12 +0300

    [2336/2437] adaptive header

---
+++ src/components/header/header.component.js
@@ -0,0 +1,13 @@
+import template from './header.html';
+import controller from './header.controller';
+
+const headerComponent = {
+  bindings: {
+
+  },
+  template,
+  controller,
+  controllerAs: 'headerCtrl'
+};
+
+export default headerComponent;
---
+++ src/components/header/header.controller.js
@@ -0,0 +1,6 @@
+export default class HeaderController {
+  constructor() {
+    this.name = 'header';
+  }
+}
+
