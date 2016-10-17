37f6b3adb8b21d273ef74d3886c65719e7cdde4c
Anna Pokidina <anna_pokidina@epam.com>
2016-10-17 14:21:15 +0300

    fix

--- src/components/header/header.spec.js
+++ src/components/header/header.spec.js
@@ -1,4 +1,3 @@
 import HeaderModule from './header'
 import HeaderController from './header.controller';
-import HeaderComponent from './header.component';
 import HeaderTemplate from './header.html';
