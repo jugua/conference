2bdb1a1b8360edef50b9e6c8fd7e91757e493881
Anna Pokidina <anna_pokidina@epam.com>
2016-10-17 14:24:07 +0300

    fix

--- src/services/users/users.spec.js
+++ src/services/users/users.spec.js
@@ -1,18 +1,3 @@
 import UsersModule from './users'
 import UsersService from './users.service';
 
-describe('Users', () => {
-    let $rootScope, makeController;
-
-    beforeEach(window.module('app'));
-
-    describe('Service', () => {
-        // component/directive specs
-        let service = UsersService();
-
-
-        it('the name property has the correct value', () => {
-          //  expect(service.name).toBe('users');
-        });
-    });
-});
