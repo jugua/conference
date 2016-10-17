1e949608b6db74bc389d46ba5e932cae8bcdcfa1
Anna Pokidina <anna_pokidina@epam.com>
2016-10-17 14:18:23 +0300

    fix

--- src/components/account-page/account-page.spec.js
+++
@@ -1,4 +0,0 @@
-import Account-pageModule from './account-page'
-import Account-pageController from './account-page.controller';
-import Account-pageComponent from './account-page.component';
-import Account-pageTemplate from './account-page.html';
