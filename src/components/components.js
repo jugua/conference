06178b0e21630e416019b04abb9da3f9ff5036b8
Ivan Tkachuk <ivan_tkachuk@epam.com>
2016-10-18 16:39:56 +0300

    create basic tabs layout

--- src/components/components.js
+++ src/components/components.js
@@ -3,7 +3,8 @@ import headerComponent from './header/header';
 import homeComponent from './home/home';
 import signInComponent from './sign-in/sign-in';
 import accountPageComponent from './account-page/account-page';
+import tabsComponent from './tabs/tabs';
 
 export default (app) => {
-  include_all_modules([headerComponent, homeComponent, signInComponent, accountPageComponent], app);
+  include_all_modules([headerComponent, homeComponent, signInComponent, accountPageComponent, tabsComponent], app);
 };
