d890f26307675d04569f4324b066e282c3d9ef2f
Dmytro Poizd <Dmytro_Poizd@epam.com>
2016-10-20 13:10:41 +0300

    2240/2240 Add functionality for tabs

--- src/components/my-info/my-info.js
+++ src/components/my-info/my-info.js
@@ -3,7 +3,7 @@ import myInfoComponent from './my-info.component';
 export default (app) => {
   app.config(($stateProvider) => {
     $stateProvider
-      .state('myInfo', {
+      .state('header.tabs.myInfo', {
         url: '/my-info',
         template: '<my-info></my-info>'
       });
