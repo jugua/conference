4deb4a1548df2b102f4f6022228b02fa35fa4686
Artem Pryzhkov <artem_pryzhkov@epam.com>
2016-11-10 16:28:58 +0200

    add status annotation

--- conference-management-backend/src/main/java/ua/rd/cm/domain/Status.java
+++ conference-management-backend/src/main/java/ua/rd/cm/domain/Status.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Artem_Pryzhkov
  */
@@ -13,7 +16,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(exclude = "id")
+@Entity
+@Table(name = "status")
 public class Status {
+    @TableGenerator(
+            name = "statusGen",
+            table = "status_id_gen",
+            pkColumnName = "gen_key",
+            valueColumnName = "gen_value",
+            pkColumnValue = "status_id",
+            allocationSize = 1
+    )
+
+    @Id
+    @Column(name = "status_id")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "statusGen")
     private Long id;
+
+    @NotNull
+    @Column(name = "status_name", nullable = false)
     private String name;
 }
