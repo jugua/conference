badc801863d6305de6cf414248e3b8a19bfde11d
Artem Pryzhkov <artem_pryzhkov@epam.com>
2016-11-10 16:32:53 +0200

    add level annotation

--- conference-management-backend/src/main/java/ua/rd/cm/domain/Level.java
+++ conference-management-backend/src/main/java/ua/rd/cm/domain/Level.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Artem_Pryzhkov
  */
@@ -13,7 +16,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(exclude = "id")
+@Entity
+@Table(name = "level")
 public class Level {
+    @TableGenerator(
+            name = "levelGen",
+            table = "level_id_gen",
+            pkColumnName = "gen_key",
+            valueColumnName = "gen_value",
+            pkColumnValue = "level_id",
+            allocationSize = 1
+    )
+
+    @Id
+    @Column(name = "level_id")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "levelGen")
     private Long id;
+
+    @NotNull
+    @Column(name = "level_name", nullable = false)
     private String name;
 }
