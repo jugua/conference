2ddb2a8a64628964b43bcf2aff582696fbd6071f
Yaroslav <Yaroslav_Revin@epam.com>
2016-11-10 13:05:08 +0200

    add basic web configuration

---
+++ conference-management-backend/src/main/java/ua/rd/cm/config/WebMvcConfig.java
@@ -0,0 +1,12 @@
+package ua.rd.cm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author Yaroslav_Revin
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig {
+}
