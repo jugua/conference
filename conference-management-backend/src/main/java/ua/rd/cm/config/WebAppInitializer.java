1c01046b85d7a0e11fc676f5795fdb82587af81f
Dmytro Skrypnyk <Dmytro_Skrypnyk@epam.com>
2016-11-11 11:34:29 +0200

    Add logger properties and add logger to WebAppInitializer class

--- conference-management-backend/src/main/java/ua/rd/cm/config/WebAppInitializer.java
+++ conference-management-backend/src/main/java/ua/rd/cm/config/WebAppInitializer.java
@@ -1,5 +1,6 @@
 package ua.rd.cm.config;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -8,14 +9,18 @@ import org.springframework.web.servlet.DispatcherServlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import java.util.Arrays;
 
 /**
  * @author Yaroslav_Revin
  */
 public class WebAppInitializer implements WebApplicationInitializer{
+    private Logger logger = Logger.getLogger(WebAppInitializer.class);
+
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         WebApplicationContext context = setUpContext(servletContext);
+        logger.info("LOADED BEANS " + Arrays.toString(context.getBeanDefinitionNames()));
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet", new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/*");
