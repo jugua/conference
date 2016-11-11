7fde5895db2ed7bdb74fb4e827dd8d3e43d91fc3
Dmytro Skrypnyk <Dmytro_Skrypnyk@epam.com>
2016-11-11 15:31:05 +0200

    Create controller for User

---
+++ conference-management-backend/src/main/java/ua/rd/cm/web/controller/UserController.java
@@ -0,0 +1,9 @@
+package ua.rd.cm.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+}
