6a89ebdf5fe8ec777f4e6c807a55d42ffc64e272
Yaroslav <Yaroslav_Revin@epam.com>
2016-11-15 12:49:59 +0200

    configure CustomerUserDetailsServic for propper useUser class

--- conference-management-backend/src/main/java/ua/rd/cm/services/CustomUserDetailsService.java
+++ conference-management-backend/src/main/java/ua/rd/cm/services/CustomUserDetailsService.java
@@ -1,17 +1,17 @@
 package ua.rd.cm.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 
 /**
  * @author Yaroslav_Revin
  */
+@Service
 public class CustomUserDetailsService implements UserDetailsService{
 
     @Autowired
@@ -20,7 +20,7 @@ public class CustomUserDetailsService implements UserDetailsService{
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         ua.rd.cm.domain.User user = userService.getByEmail(username);
-        UserDetails userDetails = new User(user.getEmail(), user.getPassword(), user.getRole());
+        UserDetails userDetails = new User(user.getEmail(), user.getPassword(), user.getUserRoles());
         return userDetails;
     }
 }
