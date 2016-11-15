fe37f70f8631f35e21290936c58355d38453cb79
Dmytro Skrypnyk <Dmytro_Skrypnyk@epam.com>
2016-11-15 14:07:06 +0200

    Add UserController class and method register, add RegistrationDto class

--- conference-management-backend/src/main/java/ua/rd/cm/services/SimpleUserService.java
+++ conference-management-backend/src/main/java/ua/rd/cm/services/SimpleUserService.java
@@ -2,6 +2,9 @@ package ua.rd.cm.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ua.rd.cm.domain.User;
 import ua.rd.cm.repository.UserRepository;
 import ua.rd.cm.repository.specification.user.UserByEmail;
@@ -9,10 +12,11 @@ import ua.rd.cm.repository.specification.user.UserByFirstName;
 import ua.rd.cm.repository.specification.user.UserById;
 import ua.rd.cm.repository.specification.user.UserByLastName;
 
+@Service
 public class SimpleUserService implements UserService{
-
 	private UserRepository userRepository;
-	
+
+	@Autowired
 	public SimpleUserService(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
@@ -23,6 +27,7 @@ public class SimpleUserService implements UserService{
 	}
 
 	@Override
+	@Transactional
 	public void save(User user) {
 		userRepository.saveUser(user);
 	}
@@ -53,6 +58,7 @@ public class SimpleUserService implements UserService{
 	}
 
 	@Override
+	@Transactional
 	public void updateUserProfile(User user) {
 		userRepository.updateUser(user);
 	}
---
+++ conference-management-backend/src/test/java/ua/rd/cm/web/controller/UserControllerTest.java
@@ -0,0 +1,9 @@
+package ua.rd.cm.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserControllerTest {
+    @Autowired
+    private UserController userController;
+
+}
