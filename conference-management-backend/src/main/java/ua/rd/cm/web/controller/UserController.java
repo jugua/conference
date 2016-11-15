f94e5aa261042b9ac2748c6a282d424e33233de9
Dmytro Skrypnyk <Dmytro_Skrypnyk@epam.com>
2016-11-15 18:31:33 +0200

    Add unit tests to register() method in UserController class

--- conference-management-backend/src/main/java/ua/rd/cm/web/controller/UserController.java
+++ conference-management-backend/src/main/java/ua/rd/cm/web/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ua.rd.cm.domain.Role;
 import ua.rd.cm.domain.User;
 import ua.rd.cm.services.UserService;
 import ua.rd.cm.web.controller.dto.RegistrationDto;
@@ -27,14 +26,16 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity register(@Valid @RequestBody RegistrationDto dto, BindingResult bindingResult){
+        HttpStatus status;
         if (bindingResult.hasFieldErrors() || !isPasswordConfirmed(dto)){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            status = HttpStatus.BAD_REQUEST;
+        } else if (userService.isEmailExist(dto.getEmail())){
+            status = HttpStatus.CONFLICT;
+        } else {
+            userService.save(dtoToEntity(dto));
+            status = HttpStatus.ACCEPTED;
         }
-        if (userService.isEmailExist(dto.getEmail())){
-            return new ResponseEntity(HttpStatus.CONFLICT);
-        }
-        userService.save(dtoToEntity(dto));
-        return new ResponseEntity(HttpStatus.ACCEPTED);
+        return new ResponseEntity(status);
     }
 
     private User dtoToEntity(RegistrationDto dto) {
