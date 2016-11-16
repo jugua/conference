f109cd2d356b441c8af06d175d2f4ad9f1828544
MariaLapovska <maria.lapovsky@gmail.com>
2016-11-16 16:30:28 +0200

    add log4jdbc

--- conference-management-backend/src/test/java/ua/rd/cm/config/InMemoRepositoryConfig.java
+++ conference-management-backend/src/test/java/ua/rd/cm/config/InMemoRepositoryConfig.java
@@ -14,6 +14,7 @@ import org.springframework.orm.jpa.AbstractEntityManagerFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -40,21 +41,30 @@ public class InMemoRepositoryConfig {
     private Environment env;
 
     @Bean
-    public DataSource dataSource() {
-    	Log4JdbcCustomFormatter formatter = new Log4JdbcCustomFormatter();
-        formatter.setLoggingType(LoggingType.SINGLE_LINE);
-
-        EmbeddedDatabase embeddedDatabase = new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2).build();
-        Log4jdbcProxyDataSource dataSource = new Log4jdbcProxyDataSource
-                (embeddedDatabase);
-        dataSource.setLogFormatter(formatter);
-        dataSource.setLogFormatter(new Slf4jSpyLogDelegator());
-        return dataSource;
+    public DataSource dataSourceSpied() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        return builder.setType(EmbeddedDatabaseType.H2).build();
     }
+
+    @Bean
+    public DataSource dataSource(DataSource dataSourceSpied) {
+        Log4jdbcProxyDataSource ds = new Log4jdbcProxyDataSource(dataSourceSpied);
+        Log4JdbcCustomFormatter cf = new Log4JdbcCustomFormatter();
+
+        cf.setLoggingType(LoggingType.MULTI_LINE);
+        cf.setSqlPrefix("SQL::");
+
+        ds.setLogFormatter(cf);
+
+        return ds;
+    }
+
     @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
-        return new HibernateJpaVendorAdapter();
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabase(Database.H2);
+
+        return vendorAdapter;
     }
 
     @Bean
