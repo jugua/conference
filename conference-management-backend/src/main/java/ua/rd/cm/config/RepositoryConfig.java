046355fbb335a3f2a498fdfb9daec7fdd4b70545
Artem Trybel <Artem_Trybel@epam.com>
2016-11-21 12:49:44 +0200

    fix

--- conference-management-backend/src/main/java/ua/rd/cm/config/RepositoryConfig.java
+++ conference-management-backend/src/main/java/ua/rd/cm/config/RepositoryConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.jpa.AbstractEntityManagerFactoryBean;
@@ -30,26 +31,28 @@ import java.util.Properties;
 })
 @EnableTransactionManagement
 public class RepositoryConfig {
-	
-	private Logger logger = Logger.getLogger(RepositoryConfig.class);
-
-    @Autowired
-    private Environment env;
     
-    @PostConstruct
-    public void init() {
-    	System.out.println(env.getProperty("url") + " : " + env.getProperty("user"));
-    }
-
+    @Value("#{driver}")
+    private String driver;
+    
+    @Value("#{url}")
+    private String url;
+    
+    @Value("#{user}")
+    private String user;
+    
+    @Value("#{password}")
+    private String password;
+    
     @Bean(destroyMethod = "close")
     public DataSource dataSource() {
         BasicDataSource ds = new BasicDataSource();
         	
-        ds.setDriverClassName(env.getProperty("driver"));
-        ds.setUrl(env.getProperty("url"));
-        ds.setUsername(env.getProperty("user"));
-        ds.setPassword(env.getProperty("password"));
-        logger.info(env.getProperty("url") + " : " + env.getProperty("user"));
+        ds.setDriverClassName(driver);
+        ds.setUrl(url);
+        ds.setUsername(user);
+        ds.setPassword(password);
+        
         return ds;
     }
 
@@ -62,10 +65,10 @@ public class RepositoryConfig {
     public Properties jpaProperties() {
         Properties properties = new Properties();
 
-        properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
-        properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
-        properties.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-        properties.setProperty("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
+//        properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+//        properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+//        properties.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+//        properties.setProperty("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
 
         return properties;
     }
