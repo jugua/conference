9bb3c657abdf491248183702b857a74530cbad1c
OlyaaIvanova <olya.ivanova.kiev@gmail.com>
2016-11-28 10:53:07 +0200

    Add UserInfoDto

---
+++ webapp/src/main/java/ua/rd/cm/web/controller/dto/UserInfoDto.java
@@ -0,0 +1,55 @@
+package ua.rd.cm.web.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Olha_Melnyk
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoDto {
+
+    @NotNull
+    @JsonProperty("bio")
+    @Size(min = 1, max = 2000)
+    private String userInfoShortBio;
+
+    @NotNull
+    @JsonProperty("job")
+    @Size(min = 1, max = 256)
+    private String userInfoJobTitle;
+
+    @NotNull
+    @JsonProperty("past")
+    @Size(min = 1, max = 1000)
+    private String userInfoPastConference;
+
+    @JsonProperty("photo")
+    private String photo;
+
+    @JsonProperty("company")
+    private String userInfoCompany;
+
+    @JsonProperty("linkedin")
+    private String linkedIn;
+
+    @JsonProperty("twitter")
+    private String twitter;
+
+    @JsonProperty("facebook")
+    private String facebook;
+
+    @JsonProperty("blog")
+    private String blog;
+
+    @JsonProperty("info")
+    private String userInfoAdditionalInfo;
+
+}
