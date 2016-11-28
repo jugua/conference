d14878256ab3dd93a92a27ab7bcad8be333c7621
Viacheslav DOmin <viacheslav.domin@epam.com>
2016-11-28 14:35:20 +0200

    editname markup

--- src/components/account-page/account-page.controller.js
+++ src/components/account-page/account-page.controller.js
@@ -1,6 +1,12 @@
 export default class AccountPageController {
   constructor() {
+    'ngInject';
+
     this.controler = 'controller';
+    this.editName = false;
+  }
+  showEditname() {
+    this.editName = true;
   }
 }
 
