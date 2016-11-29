dec5756f73ef7bb7cccbb0dce45787222483b515
Artem Pryzhkov <artem_pryzhkov@epam.com>
2016-11-29 10:28:49 +0200

    refactor udload method and add messages

--- webapp/src/main/java/ua/rd/cm/web/controller/PhotoController.java
+++ webapp/src/main/java/ua/rd/cm/web/controller/PhotoController.java
@@ -4,21 +4,16 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ua.rd.cm.domain.User;
 import ua.rd.cm.services.UserService;
 import ua.rd.cm.web.controller.dto.MessageDto;
 import ua.rd.cm.web.controller.dto.PhotoDto;
-import ua.rd.cm.web.controller.dto.RegistrationDto;
-import ua.rd.cm.web.controller.dto.UserDto;
 
-import javax.validation.Valid;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.security.Principal;
+import java.util.Properties;
 
 @RestController
 @RequestMapping("/api/user/current/photo")
@@ -26,6 +21,8 @@ public class PhotoController {
     private ModelMapper mapper;
     private UserService userService;
 
+    public static final String ROOT="root";
+    public static final String FOLDER="folder";
     @Autowired
     public PhotoController(ModelMapper mapper, UserService userService) {
         this.mapper = mapper;
@@ -35,80 +32,77 @@ public class PhotoController {
     @PostMapping
     public ResponseEntity upload(PhotoDto photoDto, Principal principal) {
 
+        MessageDto message = new MessageDto();
+        HttpStatus status;
+
         MultipartFile file = photoDto.getFile();
+        User currentUser = userService.getByEmail(principal.getName());
 
         if (!file.isEmpty()) {
-            System.out.println("lol");
+
+            if(file.getSize()>2097152){
+                message.setError("maxSize");
+                status=HttpStatus.PAYLOAD_TOO_LARGE;
+                return ResponseEntity.status(status).body(message);
+            }
 
             try {
                 byte[] bytes = file.getBytes();
+                File dir = createDir();
+                File serverFile = putFileIntoDir(currentUser, bytes, dir);
 
-                // Creating the directory to store file
-                String rootPath = "C://";
-
-                File dir = new File(rootPath + "tmpFiles");
-                if (!dir.exists()) {
-                    dir.mkdirs();
-                }
-
-                // Create the file on server
-                File serverFile = new File(dir.getAbsolutePath()
-                        + File.separator + "photo.jpg");
-                BufferedOutputStream stream = new BufferedOutputStream(
-                        new FileOutputStream(serverFile));
-                stream.write(bytes);
-                stream.close();
-
-                User currentUser = userService.getByEmail(principal.getName());
                 currentUser.setPhoto(serverFile.getAbsolutePath());
                 userService.updateUserProfile(currentUser);
 
-                //200 { "answer": <url>} - on success upload or update photo
-                return new ResponseEntity(HttpStatus.OK);
-            } catch (Exception e) {
-                return null;
+                message.setStatus(serverFile.getAbsolutePath());
+                status=HttpStatus.OK;
+                return ResponseEntity.status(status).body(message);
+            } catch (IOException e) {
+                return new ResponseEntity(HttpStatus.FORBIDDEN);
             }
         } else {
-            return null;
+            message.setError("save");
+            status=HttpStatus.BAD_REQUEST;
+            return ResponseEntity.status(status).body(message);
         }
     }
+
+    private File createDir() throws IOException {
+        Properties propFile=getFromPropFile();
+        File dir = new File(propFile.getProperty(ROOT) + propFile.getProperty(FOLDER));
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        return dir;
+    }
+
+    private File putFileIntoDir(User currentUser, byte[] bytes, File dir) throws IOException {
+        File serverFile = new File(dir.getAbsolutePath()
+                + File.separator + currentUser.getId()+".jpg");
+        BufferedOutputStream stream = new BufferedOutputStream(
+                new FileOutputStream(serverFile));
+        stream.write(bytes);
+        stream.close();
+        return serverFile;
+    }
+
+    private Properties getFromPropFile() throws IOException {
+        Properties propFile=new Properties();
+        InputStream input = null;
+        try {
+            input = new FileInputStream("webapp/src/main/resources/path.properties");
+            propFile.load(input);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return propFile;
+    }
+
 
 //    @DeleteMapping
 //    public ResponseEntity delete(@Valid @RequestBody RegistrationDto dto,
 //                            BindingResult bindingResult){
 //    }
 
-    /*
-
-     Method : POST     -  saves current user photo
-     Headers: token : <token> - for Node.js only
-     Body:  file
-
-    Answer
-      400  { "error": "save"} - any error of saving file on server
-     401 { "error": <error>} in case no token in headers
-     403 {error} - JSON with any other type of db or server error                description
-     413 { "error": "maxSize"} - if file size more then 2mb
-     415 { "error": "pattern"} - filetype not jpe(g), gif, png
-     200 { "answer": <url>} - on success upload or update photo (current          user photo field have link to new photo) ; url   - relative path to      new image saved in db
-
-    Error types:
-     "no-current-user" - invalid  token
-     "no-token"  - no token (not authorized)
-
-
-    Method : DELETE    -  delete current user photo
-     Headers: token : <token> - for Node.js only
-
-    Answer
-     400  { "error": "delete"} - any error of deleting file on server
-     401 { "error": <error>} in case no token in headers
-     403 {error} - JSON with any other type of db or server error                description
-     200 { } - on success delete photo
-
-    Error types:
-     "no-current-user" - invalid  token
-     "no-token"  - no token (not authorized)
-
-     */
 }
--- webapp/src/main/java/ua/rd/cm/web/controller/dto/PhotoDto.java
+++ webapp/src/main/java/ua/rd/cm/web/controller/dto/PhotoDto.java
@@ -2,11 +2,16 @@ package ua.rd.cm.web.controller.dto;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 /**
  * @author Mariia Lapovska
  */
 public class PhotoDto {
 
+//    @Size(max=2097152)
+//    @Pattern(regexp = "/jp(e)?g|gif|png$/")
     private MultipartFile file;
 
     public MultipartFile getFile() {
