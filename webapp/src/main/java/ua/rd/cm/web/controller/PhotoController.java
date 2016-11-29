d4da3575418d2fdd83482949dc69a7b7d27b501e
Artem Pryzhkov <artem_pryzhkov@epam.com>
2016-11-29 12:35:46 +0200

    add format

--- webapp/src/main/java/ua/rd/cm/web/controller/PhotoController.java
+++ webapp/src/main/java/ua/rd/cm/web/controller/PhotoController.java
@@ -21,9 +21,10 @@ public class PhotoController {
     private ModelMapper mapper;
     private UserService userService;
 
-    public static final String ROOT="root";
-    public static final String FOLDER="folder";
-    public static final long MAX_SIZE=2097152;
+    public static final String ROOT = "root";
+    public static final String FOLDER = "folder";
+    public static final long MAX_SIZE = 2097152;
+
     @Autowired
     public PhotoController(ModelMapper mapper, UserService userService) {
         this.mapper = mapper;
@@ -41,40 +42,41 @@ public class PhotoController {
 
         if (!file.isEmpty()) {
 
-            if(file.getSize()>MAX_SIZE){
+            if (file.getSize() > MAX_SIZE) {
                 message.setError("maxSize");
-                status=HttpStatus.PAYLOAD_TOO_LARGE;
+                status = HttpStatus.PAYLOAD_TOO_LARGE;
                 return ResponseEntity.status(status).body(message);
             }
-            if(file.getName().matches("([^\\s]+(\\.(?i)(jp(e)?g|gif|png))$)")){
+            if (file.getName().matches("([^\\s]+(\\.(?i)(jp(e)?g|gif|png))$)")) {
                 message.setError("pattern");
-                status=HttpStatus.UNSUPPORTED_MEDIA_TYPE;
+                status = HttpStatus.UNSUPPORTED_MEDIA_TYPE;
                 return ResponseEntity.status(status).body(message);
             }
 
             try {
                 byte[] bytes = file.getBytes();
                 File dir = createDir();
-                File serverFile = putFileIntoDir(currentUser, bytes, dir);
+                File serverFile = putFileIntoDir(currentUser, bytes, dir,
+                        getImageFormat(file.getOriginalFilename()));
 
                 currentUser.setPhoto(serverFile.getAbsolutePath());
                 userService.updateUserProfile(currentUser);
 
                 message.setStatus(serverFile.getAbsolutePath());
-                status=HttpStatus.OK;
+                status = HttpStatus.OK;
                 return ResponseEntity.status(status).body(message);
             } catch (IOException e) {
                 return new ResponseEntity(HttpStatus.FORBIDDEN);
             }
         } else {
             message.setError("save");
-            status=HttpStatus.BAD_REQUEST;
+            status = HttpStatus.BAD_REQUEST;
             return ResponseEntity.status(status).body(message);
         }
     }
 
     private File createDir() throws IOException {
-        Properties propFile=getFromPropFile();
+        Properties propFile = getFromPropFile();
         File dir = new File(propFile.getProperty(ROOT) + propFile.getProperty(FOLDER));
         if (!dir.exists()) {
             dir.mkdirs();
@@ -82,9 +84,9 @@ public class PhotoController {
         return dir;
     }
 
-    private File putFileIntoDir(User currentUser, byte[] bytes, File dir) throws IOException {
+    private File putFileIntoDir(User currentUser, byte[] bytes, File dir, String format) throws IOException {
         File serverFile = new File(dir.getAbsolutePath()
-                + File.separator + currentUser.getId()+".jpg");
+                + File.separator + currentUser.getId() + format);
         BufferedOutputStream stream = new BufferedOutputStream(
                 new FileOutputStream(serverFile));
         stream.write(bytes);
@@ -93,7 +95,7 @@ public class PhotoController {
     }
 
     private Properties getFromPropFile() throws IOException {
-        Properties propFile=new Properties();
+        Properties propFile = new Properties();
         InputStream input = null;
         try {
             input = new FileInputStream("webapp/src/main/resources/path.properties");
@@ -105,8 +107,15 @@ public class PhotoController {
         return propFile;
     }
 
-    private String getImageFormat(String fileName){
-        return null;
+    private String getImageFormat(String fileName) {
+        System.out.println("fileName=" + fileName);
+        int index = fileName.lastIndexOf('.');
+        System.out.println("index=" + index);
+        if (index == -1) {
+            return null;
+        }
+        String format = fileName.substring(index);
+        return format;
     }
 
 //    @DeleteMapping
