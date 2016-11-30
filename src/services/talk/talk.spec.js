f5cd3c01489d5642605f00eb34e7d3c1be06a7b0
Anna Pokidina <anna_pokidina@epam.com>
2016-11-30 18:25:16 +0200

    init

--- src/services/talk/talk.spec.js
+++ src/services/talk/talk.spec.js
@@ -1,2 +1,32 @@
 import TalkService from './talk.service';
 
+describe('Talk Service', () => {
+  let sut;
+  let q;
+  let LocalStorage;
+
+  beforeEach(inject((_$q_) => {
+
+    q = _$q_;
+    mockResource = {
+      add: () => {
+        deferred = $q.defer();
+        deferred.resolve({id: 1, :name:'test'});
+        return deferred.promise;
+      },
+      getAll: () => {
+        deferred = $q.defer();
+        deferred.resolve({id: 1, :name:'test'});
+        return deferred.promise;
+      }
+    };
+
+    sut = new TalkService(LocalStorage, mockResource);
+  }));
+
+
+  it('TalkService has method clear', () => {
+
+  });
+});
+
