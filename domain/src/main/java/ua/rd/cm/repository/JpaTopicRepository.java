f77af21c30a5a7638156fd81da5d9f8d0bae81fe
OlyaaIvanova <olya.ivanova.kiev@gmail.com>
2016-12-01 13:37:12 +0200

    Add TopicRepository and its implementation

---
+++ domain/src/main/java/ua/rd/cm/repository/JpaTopicRepository.java
@@ -0,0 +1,34 @@
+package ua.rd.cm.repository;
+
+import ua.rd.cm.domain.Topic;
+import ua.rd.cm.repository.specification.Specification;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * @author Mariia Lapovska
+ */
+public class JpaTopicRepository implements TopicRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void saveTopic(Topic topic) {
+        em.persist(topic);
+    }
+
+    @Override
+    public List<Topic> findAll() {
+        return em.createQuery("SELECT t FROM Topic t", Topic.class)
+                .getResultList();
+    }
+
+    @Override
+    public List<Topic> findBySpecification(Specification<Topic> spec) {
+        return em.createQuery("SELECT t FROM Topic t WHERE " + spec
+                .toSqlClauses(), Topic.class).getResultList();
+    }
+}
