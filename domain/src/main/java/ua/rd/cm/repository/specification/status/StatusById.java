5e030f894c3a18223abcb1d0156f922e0ca52b58
OlyaaIvanova <olya.ivanova.kiev@gmail.com>
2016-12-01 13:35:21 +0200

    Add Status specifications

---
+++ domain/src/main/java/ua/rd/cm/repository/specification/status/StatusById.java
@@ -0,0 +1,26 @@
+package ua.rd.cm.repository.specification.status;
+
+import ua.rd.cm.domain.Status;
+import ua.rd.cm.repository.specification.Specification;
+
+/**
+ * @author Olha_Melnyk
+ */
+public class StatusById implements Specification<Status> {
+
+    private Long id;
+
+    public StatusById(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toSqlClauses() {
+        return String.format(" s.id = '%s' ", id);
+    }
+
+    @Override
+    public boolean test(Status status) {
+        return status.getId().equals(id);
+    }
+}
