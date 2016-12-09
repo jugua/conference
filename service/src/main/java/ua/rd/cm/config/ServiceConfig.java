67c5a740103b67527bf1a54d439d503fc234ebe6
Mariia_Lapovska <Mariia_Lapovska@epam.com>
2016-12-09 06:12:55 +0200

    add mail service and configs for email

--- service/src/main/java/ua/rd/cm/config/ServiceConfig.java
+++ service/src/main/java/ua/rd/cm/config/ServiceConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 
 import javax.persistence.EntityManagerFactory;
 
@@ -27,5 +30,31 @@ public class ServiceConfig {
         jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
 
         return jpaTransactionManager;
+    }
+
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//        mailSender.setUsername("Your-gmail-id");
+//        mailSender.setPassword("Your-gmail-password");
+//
+//        Properties javaMailProperties = new Properties();
+//        javaMailProperties.put("mail.smtp.starttls.enable", "true");
+//        javaMailProperties.put("mail.smtp.auth", "true");
+//        javaMailProperties.put("mail.transport.protocol", "smtp");
+//        javaMailProperties.put("mail.debug", "true");
+//
+//        mailSender.setJavaMailProperties(javaMailProperties);
+        return mailSender;
+    }
+
+    @Bean
+    public FreeMarkerConfigurationFactoryBean getFreeMarkerConfiguration() {
+        FreeMarkerConfigurationFactoryBean bean = new FreeMarkerConfigurationFactoryBean();
+        bean.setTemplateLoaderPath("/fmtemplates/");
+        return bean;
     }
 }
