ee90adabadae5a9ebdc1c9268519a12eafce5625
Dmytro Skrypnyk <Dmytro_Skrypnyk@epam.com>
2016-12-09 15:14:17 +0200

    Setup Link.class

--- domain/src/main/java/ua/rd/cm/domain/Link.java
+++ domain/src/main/java/ua/rd/cm/domain/Link.java
@@ -1,7 +1,39 @@
 package ua.rd.cm.domain;
 
-/**
- * Created by Dmytro_Skrypnyk on 12/9/2016.
- */
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = "id")
+@Entity
+@SequenceGenerator(name = "seqLinkGen", allocationSize = 1,
+        sequenceName = "link_seq")
 public class Link {
+
+    @Id
+    @Column(name = "link_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqLinkGen")
+    private Long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private LinkStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime time;
+
+    public enum LinkStatus {
+        VALID, EXPIRED
+    }
 }
