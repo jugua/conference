943c63429e6b5c44b971bb32e375672225d8f830
Lev Serba <lev_serba@epam.com>
2016-12-13 15:10:54 +0200

    [framework] modified settings for email story

---
+++ CM/src/test/java/com/epam/cm/dto/SettingsDTO.java
@@ -0,0 +1,16 @@
+package com.epam.cm.dto;
+
+/**
+ * Created by Lev_Serba on 12/13/2016.
+ */
+public class SettingsDTO {
+   private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
