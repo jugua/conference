ca200084782adf5f3aa80ef07787d5fa9017eb6e
Artem Pryzhkov <artem_pryzhkov@epam.com>
2016-12-15 16:35:23 +0200

    photoController test mock

--- webapp/src/test/java/ua/rd/cm/web/controller/PhotoControllerTest.java
+++ webapp/src/test/java/ua/rd/cm/web/controller/PhotoControllerTest.java
@@ -65,11 +65,8 @@ public class PhotoControllerTest {
         PhotoDto photoDto=new PhotoDto();
         try {
             File file= new File("src/test/resources/trybel_master.JPG");
-            System.out.println(file.getAbsolutePath());
             FileInputStream fileInputStream=new FileInputStream(file);
-            System.out.println(fileInputStream.toString());
             MockMultipartFile mockFile=new MockMultipartFile("trybel_master",fileInputStream);
-            System.out.println(mockFile.getName());
             photoDto.setFile(mockFile);
         } catch (IOException e) {
             e.printStackTrace();
@@ -77,69 +74,9 @@ public class PhotoControllerTest {
         return photoDto;
     }
 
-//    @Test
-//    public void correctPhoto() throws Exception {
-////        Role speaker = createSpeakerRole();
-////        UserInfo info = createUserInfo();
-////        User user = createUser(speaker, info);
-////        Principal correctPrincipal = () -> user.getEmail();
-//        mockMvc.perform(post(API_PHOTO)
-//                .contentType(MediaType.APPLICATION_JSON_UTF8)
-//        ).andExpect(status().isAccepted());
-//    }
-//    @Test
-//    public void addCorrectPhoto(){
-//
-
-
     @Test
-    public void nullPhoto(){
-        correctPhotoDto.setFile(null);
-        checkForBadRequest(API_PHOTO, RequestMethod.POST, correctPhotoDto);
-    }
-    private void checkForBadRequest(String uri, RequestMethod method, Object dto) {
-        try {
-            if (method == RequestMethod.POST) {
-                mockMvc.perform(post(uri)
-                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .content(convertObjectToJsonBytes(dto))
-                ).andExpect(status().isBadRequest());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    private byte[] convertObjectToJsonBytes(Object object) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        return mapper.writeValueAsBytes(object);
-    }
+    public void run(){
 
-    private User createUser(Role role , UserInfo info){
-        Set<Role> roles = new HashSet<>();
-        roles.add(role);
-        User user = new User(1L, "FirstName", "LastName", alreadyRegisteredEmail, "src/test/trybel_master.JPG", "url",
-                User.UserStatus.CONFIRMED, info, roles);
-        return user;
     }
-
-    private Role createSpeakerRole(){
-        return new Role(1L, "SPEAKER");
-    }
-    private UserInfo createUserInfo(){
-        ContactType contactType = new ContactType(1L, "LinkedIn");
-        ContactType contactType2 = new ContactType(2L, "Twitter");
-        ContactType contactType3 = new ContactType(3L, "FaceBook");
-        ContactType contactType4 = new ContactType(4L, "Blog");
-
-        Map<ContactType, String> contacts = new HashMap<ContactType, String>(){{
-            put(contactType, "LinkedIn");
-            put(contactType2, "Twitter");
-            put(contactType3, "FaceBook");
-            put(contactType4, "Blog");
-        }};
-        return new UserInfo(1L, "bio", "job", "pastConference", "EPAM", contacts, "addInfo");
-    }
-
 
 }
