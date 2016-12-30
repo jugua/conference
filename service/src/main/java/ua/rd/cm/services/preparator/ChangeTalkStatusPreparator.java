fd20ecab789a321311364bce055e01fd04017052
Dmytro Skrypnyk <Dmytro_Skrypnyk@epam.com>
2016-12-30 15:16:34 +0200

    Add preparators for organisers notification

--- service/src/main/java/ua/rd/cm/services/preparator/ChangeTalkStatusPreparator.java
+++ service/src/main/java/ua/rd/cm/services/preparator/ChangeTalkStatusPreparator.java
@@ -4,18 +4,17 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 
 import javax.mail.internet.MimeMessage;
 
-public class ChangePasswordPreparator extends CustomMimeMessagePreparator {
-
+public class ChangeTalkStatusPreparator extends CustomMimeMessagePreparator {
     @Override
     public String getTemplateName() {
-        return "change_password_template.ftl";
+        return null;
     }
 
     @Override
     public void prepare(MimeMessage mimeMessage) throws Exception {
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
 
-        helper.setSubject("Your password has been changed");
+        helper.setSubject("A talk's status has been updated");
         helper.setFrom("support@conference.com");
         helper.setTo((String)model.get("email"));
         helper.setText(getFreeMarkerTemplateContent(model), true);
