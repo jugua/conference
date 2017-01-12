49ca816bebe23c827b1daa384167d7fcee7de05b
Artem Pryzhkov <artem_pryzhkov@epam.com>
2017-01-12 15:12:32 +0200

    fix db connection

--- domain/src/main/java/ua/rd/cm/config/RepositoryConfig.java
+++ domain/src/main/java/ua/rd/cm/config/RepositoryConfig.java
@@ -28,8 +28,8 @@ public class RepositoryConfig {
 
         ds.setDriverClassName("com.mysql.jdbc.Driver");
         ds.setUrl("jdbc:mysql://localhost:3306/conference_management");
-        ds.setUsername("root");
-        ds.setPassword("Artem11063880AA");
+        ds.setUsername("trybel_master");
+        ds.setPassword("password");
 
         return ds;
     }
