c01d8063ff98961129889a1441f212dd64cc4b4b
Anthony <zaknofein2@gmail.com>
2017-01-24 19:21:31 +0200

    Replace hardcoded jdbc properties

--- domain/src/main/java/ua/rd/cm/config/RepositoryConfig.java
+++ domain/src/main/java/ua/rd/cm/config/RepositoryConfig.java
@@ -1,15 +1,17 @@
 package ua.rd.cm.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.jpa.AbstractEntityManagerFactoryBean;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 import javax.sql.DataSource;
 
 @Configuration
@@ -17,15 +19,19 @@ import javax.sql.DataSource;
 @EnableTransactionManagement
 @PropertySource("classpath:jdbc.properties")
 public class RepositoryConfig {
+    private static final String DRIVER_PROP = "driver";
+    private static final String URL_PROP = "url";
+    private static final String USER_PROP = "user";
+    private static final String PASSWORD_PROP = "password";
 
     @Bean(destroyMethod = "close")
-    public DataSource dataSource() {
+    public DataSource dataSource(Environment environment) {
         BasicDataSource ds = new BasicDataSource();
 
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/conference_management");
-        ds.setUsername("trybel_master");
-        ds.setPassword("password");
+        ds.setDriverClassName(environment.getProperty(DRIVER_PROP));
+        ds.setUrl(environment.getProperty(URL_PROP));
+        ds.setUsername(environment.getProperty(USER_PROP));
+        ds.setPassword(environment.getProperty(PASSWORD_PROP));
         return ds;
     }
 
