b9fc9669a5c3c703adc90855e932af1f69db1203
Igor_Pidgurskyi <Igor_Pidgurskyi@epam.com>
2017-01-27 11:21:15 +0200

    PhotoService changed to FileService

--- service/src/test/java/ua/rd/cm/services/FileStorageServiceImplTest.java
+++ service/src/test/java/ua/rd/cm/services/FileStorageServiceImplTest.java
@@ -18,8 +18,8 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FileStorageServiceImplTest {
-    private static final String PARENTFOLDER = System.getenv("TEMP");
-    private static final String FOLDER = PARENTFOLDER + File.separator + "fs" + File.separator;
+    private static final String PARENTFOLDER = System.getProperty("java.io.tmpdir");
+    private static final String FOLDER = PARENTFOLDER + "fs" + File.separator;
     private static FileStorageService fileStorageService;
     private MockMultipartFile mockedFile = mock(MockMultipartFile.class);
 
