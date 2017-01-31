e2fd94289271da93d17ee214a57d354c8dc504d4
milinchuk <anastasia.milinchuk@gmail.com>
2017-01-31 13:23:34 +0200

    Added new classes: UserByRole, OrderByLastName, change UserByRoleJoin

--- domain/src/main/java/ua/rd/cm/repository/specification/user/UserByRole.java
+++ domain/src/main/java/ua/rd/cm/repository/specification/user/UserByRole.java
@@ -1,15 +1,16 @@
 package ua.rd.cm.repository.specification.user;
 
+import ua.rd.cm.domain.Role;
 import ua.rd.cm.domain.User;
 import ua.rd.cm.repository.specification.Specification;
 
 /**
- * Created by click on 1/30/2017.
+ * Created by click on 1/31/2017.
  */
-public class UserAddRoleToQuery implements Specification<User> {
+public class UserByRole implements Specification<User> {
     private String roleName;
 
-    public UserAddRoleToQuery(String roleName) {
+    public UserByRole(String roleName) {
         this.roleName = roleName;
     }
 
@@ -20,6 +21,11 @@ public class UserAddRoleToQuery implements Specification<User> {
 
     @Override
     public boolean test(User user) {
+        for (Role currentRole : user.getUserRoles()) {
+            if (currentRole.getName().equals(roleName)) {
+                return true;
+            }
+        }
         return false;
     }
 }
