26c57f8cb6126aba0d1169b514f7ce6778f54fc8
Anton_Bondarenko <Anton_Bondarenko@epam.com>
2017-02-08 14:21:36 +0200

    Add ConferenceService interface

---
+++ service/src/main/java/ua/rd/cm/services/ConferenceService.java
@@ -0,0 +1,24 @@
+package ua.rd.cm.services;
+
+import ua.rd.cm.domain.Conference;
+
+import java.util.List;
+
+public interface ConferenceService {
+
+    void save(Conference conference);
+
+    void update(Conference conference);
+
+    void remove(Conference conference);
+
+    List<Conference> findAll();
+
+    List<Conference> findPast();
+
+    List<Conference> findOpen();
+
+    List<Conference> findOngoing();
+
+    List<Conference> findUntagged();
+}
