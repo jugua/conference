0873ad065f1b7b4016e4f3f8cb72c0d0a72ebc33
Anastasiia_Milinchuk <Anastasiia_Milinchuk@epam.com>
2017-02-08 17:00:27 +0200

    implementationconferenceService

--- service/src/main/java/ua/rd/cm/services/SimpleConferenceService.java
+++ service/src/main/java/ua/rd/cm/services/SimpleConferenceService.java
@@ -1,7 +1,40 @@
 package ua.rd.cm.services;
 
+import ua.rd.cm.domain.Conference;
+
+import java.util.List;
+
 /**
  * Created by Anastasiia_Milinchuk on 2/8/2017.
  */
 public class SimpleConferenceService implements ConferenceService {
+    @Override
+    public void save(Conference conference) {
+        
+    }
+
+    @Override
+    public void update(Conference conference) {
+
+    }
+
+    @Override
+    public void remove(Conference conference) {
+
+    }
+
+    @Override
+    public List<Conference> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Conference> findPast() {
+        return null;
+    }
+
+    @Override
+    public List<Conference> findUpcoming() {
+        return null;
+    }
 }
