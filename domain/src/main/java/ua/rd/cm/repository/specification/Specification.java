5c60f9d9e09abb04582913e72c7d3dbf7adef62b
Anton_Bondarenko <Anton_Bondarenko@epam.com>
2017-02-09 11:13:17 +0200

    Add default method setParameters to Spec interface

--- domain/src/main/java/ua/rd/cm/repository/specification/Specification.java
+++ domain/src/main/java/ua/rd/cm/repository/specification/Specification.java
@@ -1,9 +1,13 @@
 package ua.rd.cm.repository.specification;
 
+import javax.persistence.Query;
 import java.util.function.Predicate;
 
-public interface Specification<T> extends Predicate<T>{
-	
-	String toSqlClauses();
-	
+public interface Specification<T> extends Predicate<T> {
+
+    String toSqlClauses();
+
+    default Query setParameters(Query query) {
+        return query;
+    }
 }
