cb8da26bb909b9f8962357379eea796f357cfba6
Anton_Bondarenko <Anton_Bondarenko@epam.com>
2017-02-22 16:20:07 +0200

    Remove unused import

--- webapp/src/main/java/ua/rd/cm/web/security/AuthenticationEntryPointImpl.java
+++ webapp/src/main/java/ua/rd/cm/web/security/AuthenticationEntryPointImpl.java
@@ -8,7 +8,6 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
 import java.io.IOException;
 
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
