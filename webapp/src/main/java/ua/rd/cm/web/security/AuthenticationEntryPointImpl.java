eacb281a5a4f87e15e62b63bc81f1c3bdf584cbd
Anton_Bondarenko <Anton_Bondarenko@epam.com>
2017-02-22 16:15:32 +0200

    Add authentication handlers

---
+++ webapp/src/main/java/ua/rd/cm/web/security/AuthenticationEntryPointImpl.java
@@ -0,0 +1,31 @@
+package ua.rd.cm.web.security;
+
+import lombok.Setter;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.io.IOException;
+
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+    public static final String DEFAULT_UNAUTHORIZED_MSG = "unauthorized";
+
+    @Setter
+    private int errorCode = HttpServletResponse.SC_UNAUTHORIZED;
+    @Setter
+    private String message = DEFAULT_UNAUTHORIZED_MSG;
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException)
+            throws IOException, ServletException {
+        response.setStatus(errorCode);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().append(String.format("{\"error\":\"%s\"}", message));
+    }
+}
