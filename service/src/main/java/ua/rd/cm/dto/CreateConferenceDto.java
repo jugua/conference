6b7eb51a9c78e751a3c38ccec484de8f07ca61b4
Anthony <zaknofein2@gmail.com>
2017-02-28 23:48:58 +0200

    Add CreateConferenceDto

---
+++ service/src/main/java/ua/rd/cm/dto/CreateConferenceDto.java
@@ -0,0 +1,49 @@
+package ua.rd.cm.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class CreateConferenceDto {
+
+    @NotNull
+    @Size(max = 1000)
+    @JsonProperty("title")
+    private String title;
+
+    @NotNull
+    @Size(max = 3000)
+    @JsonProperty("description")
+    private String description;
+
+    @NotNull
+    @Size(max = 256)
+    @JsonProperty("location")
+    private String location;
+
+    @JsonProperty("start_date")
+    private LocalDate startDate;
+    @JsonProperty("end_date")
+    private LocalDate endDate;
+    @JsonProperty("no_dates")
+    private boolean noDates;
+
+    @JsonProperty("cfp_start_date")
+    private LocalDate callForPaperStartDate;
+    @JsonProperty("cfp_end_date")
+    private LocalDate callForPaperEndDate;
+    @JsonProperty("cfp_no_dates")
+    private boolean noCallForPaperDates;
+
+    @JsonProperty("types")
+    private List<Long> typeIds;
+    @JsonProperty("organisers")
+    private List<Long> organiserIds;
+    @JsonProperty("topics")
+    private List<Long> topicIds;
+}
