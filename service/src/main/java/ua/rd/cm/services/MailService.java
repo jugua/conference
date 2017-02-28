d19950fd9dcec27953bf893fdc10c76f9a182e10
Igor_Pidgurskyi <Igor_Pidgurskyi@epam.com>
2017-02-28 13:19:04 +0200

    System.out removing

--- service/src/main/java/ua/rd/cm/services/MailService.java
+++ service/src/main/java/ua/rd/cm/services/MailService.java
@@ -1,7 +1,10 @@
 package ua.rd.cm.services;
 
 import freemarker.template.Configuration;
+
 import java.util.List;
+
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.MailException;
@@ -9,11 +12,12 @@ import org.springframework.mail.javamail.JavaMailSender;
 import ua.rd.cm.domain.User;
 import ua.rd.cm.services.preparator.CustomMimeMessagePreparator;
 
+@Log4j
 @Service
 public class MailService {
     private JavaMailSender mailSender;
     private Configuration freemarkerConfiguration;
-    
+
     @Autowired
     public MailService(JavaMailSender mailSender, Configuration freemarkerConfiguration) {
         this.mailSender = mailSender;
@@ -21,13 +25,13 @@ public class MailService {
     }
 
     public void sendEmail(User receiver, CustomMimeMessagePreparator preparator) {
-    	preparator.prepareModel(receiver);
-    	preparator.setFreemarkerConfiguration(freemarkerConfiguration);
-    	try {
-    		mailSender.send(preparator);
-    	} catch (MailException e) {
-    		System.out.println("so sad (");
-    	}
+        preparator.prepareModel(receiver);
+        preparator.setFreemarkerConfiguration(freemarkerConfiguration);
+        try {
+            mailSender.send(preparator);
+        } catch (MailException e) {
+            log.error("Couldn't send email:", e);
+        }
     }
 
     public void notifyUsers(List<User> receivers, CustomMimeMessagePreparator preparator) {
