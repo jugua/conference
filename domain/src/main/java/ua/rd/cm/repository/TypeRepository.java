c49c853c9ce38a3262d3f4ce73f62ccc3a9c0196
Anthony <zaknofein2@gmail.com>
2017-03-04 12:44:34 +0200

    Refactor TypeRepository

--- domain/src/main/java/ua/rd/cm/repository/TypeRepository.java
+++ domain/src/main/java/ua/rd/cm/repository/TypeRepository.java
@@ -1,18 +1,6 @@
 package ua.rd.cm.repository;
 
 import ua.rd.cm.domain.Type;
-import ua.rd.cm.repository.specification.Specification;
 
-import java.util.List;
-
-/**
- * @author Mariia Lapovska
- */
-public interface TypeRepository {
-
-    void saveType(Type type);
-
-    List<Type> findAll();
-
-    List<Type> findBySpecification(Specification<Type> spec);
+public interface TypeRepository extends CrudRepository<Type> {
 }
--- domain/src/main/java/ua/rd/cm/repository/jpa/TypeRepositoryImpl.java
+++ domain/src/main/java/ua/rd/cm/repository/jpa/TypeRepositoryImpl.java
@@ -3,35 +3,12 @@ package ua.rd.cm.repository.jpa;
 import org.springframework.stereotype.Repository;
 import ua.rd.cm.domain.Type;
 import ua.rd.cm.repository.TypeRepository;
-import ua.rd.cm.repository.specification.Specification;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
-
-/**
- * @author Mariia Lapovska
- */
 @Repository
-public class TypeRepositoryImpl implements TypeRepository {
+public class TypeRepositoryImpl
+        extends AbstractJpaCrudRepository<Type> implements TypeRepository {
 
-    @PersistenceContext
-    private EntityManager em;
-
-    @Override
-    public void saveType(Type type) {
-        em.persist(type);
-    }
-
-    @Override
-    public List<Type> findAll() {
-        return em.createQuery("SELECT t FROM Type t", Type.class)
-                .getResultList();
-    }
-
-    @Override
-    public List<Type> findBySpecification(Specification<Type> spec) {
-        return em.createQuery("SELECT t FROM Type t WHERE " + spec
-                .toSqlClauses(), Type.class).getResultList();
+    public TypeRepositoryImpl() {
+        super("t", Type.class);
     }
 }
