f0bac1cf768eadee9d7be44df5ab3d75e4cdd1cb
Anton_Bondarenko <Anton_Bondarenko@epam.com>
2017-03-09 14:40:57 +0200

    Merge commit

--- service/src/main/java/ua/rd/cm/config/ServiceConfig.java
+++ service/src/main/java/ua/rd/cm/config/ServiceConfig.java
@@ -1,10 +1,6 @@
 package ua.rd.cm.config;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
