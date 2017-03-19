92c98d9a3d55373472e07cef4b39a17be226b038
Anthony <zaknofein2@gmail.com>
2017-03-19 16:14:50 +0200

    Rename sql column name for organiser fk

--- domain/src/main/java/ua/rd/cm/domain/Talk.java
+++ domain/src/main/java/ua/rd/cm/domain/Talk.java
@@ -53,10 +53,11 @@ public class Talk extends AbstractEntity {
     @Column(name = "additional_info", length = 1500)
     private String additionalInfo;
 
-    @Column(name="organiser_comment", length=1000)
+    @Column(name = "organiser_comment", length = 1000)
     private String organiserComment;
 
     @ManyToOne
+    @JoinColumn(name = "organiser_id")
     private User organiser;
 
     @ManyToOne
@@ -65,18 +66,18 @@ public class Talk extends AbstractEntity {
     @Column(name = "attached_file")
     private String pathToAttachedFile;
 
-    public boolean setStatus(TalkStatus status){
-        if(this.status==null){
-            this.status=status;
+    public boolean setStatus(TalkStatus status) {
+        if (this.status == null) {
+            this.status = status;
         }
-        if(this.status.canChangeTo(status)){
-            this.status=status;
+        if (this.status.canChangeTo(status)) {
+            this.status = status;
             return true;
         }
         return false;
     }
 
-    public boolean isValidComment(){
+    public boolean isValidComment() {
         return organiserComment != null && organiserComment.length() > 0;
     }
 
