6f9207e131306029de8f00d45f62774b2461bf31
Tymur Dadashov <Tymur_Dadashov@epam.com>
2017-08-10 13:45:22 +0300

    add request mappint '/api/registration' and empty methods registration and confirmationProcess to RegistrationController

--- webapp/src/main/java/ua/rd/cm/web/controller/RegistrationController.java
+++ webapp/src/main/java/ua/rd/cm/web/controller/RegistrationController.java
@@ -1,4 +1,16 @@
 package ua.rd.cm.web.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/registration")
 public class RegistrationController {
+
+    public void registration(){
+
+    }
+    public void confirmationProcess(){
+
+    }
 }
