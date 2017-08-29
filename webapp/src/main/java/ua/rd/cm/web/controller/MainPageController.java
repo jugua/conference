d5da31700b2b51a49943d408fac455f08fe713eb
Valeriia_Fatova <valeriia.fatova@gmail.com>
2017-08-29 14:11:06 +0300

    Added lacking getTalks, getTopics, getLanguages, getLevels to MainPageController

--- webapp/src/main/java/ua/rd/cm/web/controller/MainPageController.java
+++ webapp/src/main/java/ua/rd/cm/web/controller/MainPageController.java
@@ -1,33 +1,21 @@
 package ua.rd.cm.web.controller;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
-import org.modelmapper.ModelMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j;
 import ua.rd.cm.domain.Conference;
 import ua.rd.cm.domain.Role;
-import ua.rd.cm.domain.Talk;
-import ua.rd.cm.domain.TalkStatus;
 import ua.rd.cm.dto.*;
-import ua.rd.cm.services.ConferenceService;
-import ua.rd.cm.services.TopicService;
-import ua.rd.cm.services.TypeService;
+import ua.rd.cm.services.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.List;
 
 
 @Log4j
@@ -39,6 +27,8 @@ public class MainPageController {
     private final TypeService typeService;
     private final TopicService topicService;
     private final ConferenceService conferenceService;
+    private final LevelService levelService;
+    private final LanguageService languageService;
 
     @GetMapping("conference/upcoming")
     public ResponseEntity upcomingConferences(HttpServletRequest request) {
@@ -88,7 +78,30 @@ public class MainPageController {
         messageDto.setId(id);
         return new ResponseEntity<>(messageDto, HttpStatus.OK);
     }
-    
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("type")
+    public ResponseEntity getTypes() {
+        return new ResponseEntity<>(typeService.findAll(), HttpStatus.OK);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("topic")
+    public ResponseEntity getTopics() {
+        return new ResponseEntity<>(topicService.findAll(), HttpStatus.OK);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("level")
+    public ResponseEntity getLevels() {
+        return new ResponseEntity<>(levelService.findAll(), HttpStatus.OK);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("lang")
+    public ResponseEntity getLanguages() {
+        return new ResponseEntity<>(languageService.findAll(), HttpStatus.OK);
+    }
 
     private ResponseEntity responseEntityConferencesByRole(HttpServletRequest request, List<Conference> conferences) {
         if (request.isUserInRole(Role.ADMIN) || request.isUserInRole(Role.ORGANISER)) {
@@ -100,5 +113,5 @@ public class MainPageController {
     }
 
 
-}
+}webapp/src/main/java/ua/rd/cm/web/controller/MainPageController.java
 
