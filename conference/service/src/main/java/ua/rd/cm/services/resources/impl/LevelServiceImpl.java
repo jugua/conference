dbfd7d909c7859724ef9f92418ff41aa6277e754
Solomka <solomiya_yaremko@epam.com>
2017-09-18 18:17:38 +0300

    add lombok constructor annotation to services

--- conference/service/src/main/java/ua/rd/cm/services/resources/impl/LevelServiceImpl.java
+++ conference/service/src/main/java/ua/rd/cm/services/resources/impl/LevelServiceImpl.java
@@ -3,6 +3,7 @@ package ua.rd.cm.services.resources.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,16 +13,11 @@ import ua.rd.cm.repository.LevelRepository;
 import ua.rd.cm.services.resources.LevelService;
 
 @Service
+@AllArgsConstructor(onConstructor = @__({@Autowired}))
 public class LevelServiceImpl implements LevelService {
 
     private ModelMapper modelMapper;
     private LevelRepository levelRepository;
-
-    @Autowired
-    public LevelServiceImpl(ModelMapper modelMapper, LevelRepository levelRepository) {
-        this.modelMapper = modelMapper;
-        this.levelRepository = levelRepository;
-    }
 
     @Override
     public List<LevelDto> findAll() {
