a5d03c4742d03655bd4a90b1e4e20d464373c4c5
kris45 <khrystyna.salo.40@gmail.com>
2017-09-19 12:38:56 +0300

    add unit tests

--- conference/react-js/app/src/ComponentA/index.jsx
+++ conference/react-js/app/src/ComponentA/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './component.sass';
 
-export default class extends React.Component {
+export default class MyComponent extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 
 	render() {
 		return (
-			<h2>COMPONENT A - Folkbit</h2>
+			<p>I am not a very smart component...</p>
 		);
 	}
 }
