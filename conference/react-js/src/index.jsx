ebb030ba00c31b961395644dbf88c9daceb9d9d6
Asan Ametov <Asan_Ametov@epam.com>
2017-09-20 10:55:02 +0300

    remove app directory

--- conference/react-js/src/index.jsx
+++ conference/react-js/src/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './index.sass';
 
+import ComponentA from './ComponentA';
+import ComponentB from './ComponentB';
 
 function App() {
+  // constructor(props) {
+  //   super(props);
+  // }
+  //
+  // render() {
   return (
-    <h1>App</h1>
+    <div>
+      <h1>HI WORLD</h1>
+      <ComponentA />
+      <ComponentB />
+    </div>
   );
+  // }
 }
 
 render(<App />, document.getElementById('react-root'));
