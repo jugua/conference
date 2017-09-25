dc052a58da9300b996b051573588da2044ba6522
Andrey <likeintel9@gmail.com>
2017-09-25 11:20:06 +0300

    add RestController annotation to ResourceController class

--- conference/webapp/src/main/java/ua/rd/cm/web/controller/ResourceController.java
+++ conference/webapp/src/main/java/ua/rd/cm/web/controller/ResourceController.java
@@ -6,11 +6,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ua.rd.cm.services.businesslogic.TopicService;
 import ua.rd.cm.services.businesslogic.TypeService;
 import ua.rd.cm.services.resources.LanguageService;
 import ua.rd.cm.services.resources.LevelService;
 
+@RestController
+@RequestMapping("/")
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
 public class ResourceController {
     private final TypeService typeService;
