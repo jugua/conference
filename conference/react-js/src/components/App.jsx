e2556cc2e8a0bebdff5ef2a64ef0aa5191155e06
Maksym Pushkarov <Maksym_Pushkarov@emap.com>
2017-10-03 15:35:42 +0300

    Added route for sign-uo component

---
+++ conference/react-js/src/components/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import ForgotPassword from './../containers/Forgot-password';
+import Tabs from './Tabs';
+import Header from './Header';
+import '../scss/main.scss';
+import SignUp from '../containers/SignUpForm';
+
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+      <Route
+        path="/"
+        exact
+        component={Tabs}
+      />
+      <Route
+        path="/forgot-password"
+        exact
+        component={ForgotPassword}
+      />
+      <Route
+        path="/sign-up"
+
+        component={SignUp}
+      />
+    </div>
+  </Router>
+);
+
+export default App;
