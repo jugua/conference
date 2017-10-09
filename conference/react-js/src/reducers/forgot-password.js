3283465965d4650cc1460c759a6ee7cdca7daab0
Maksym Pushkarov <maksympushkaryov@gmail.com>
2017-10-09 14:45:45 +0300

    d

--- conference/react-js/src/reducers/forgot-password.js
+++ conference/react-js/src/reducers/forgot-password.js
@@ -3,13 +3,13 @@ import actions from '../constants/actions-types';
 const { SHOW_SUCCESS_RESET_PASSWORD_MESSAGE,
   HIDE_SUCCESS_RESET_PASSWORD_MESSAGE,
 } = actions;
-const results={
+const results = {
   [SHOW_SUCCESS_RESET_PASSWORD_MESSAGE]: true,
   [HIDE_SUCCESS_RESET_PASSWORD_MESSAGE]: false,
 };
 
 const forgotPassword = (isPasswordForgottenSent = false, action) => (
-    Object.prototype.hasOwnProperty.call(results, action.type) ?
-      results[action.type] : isPasswordForgottenSent
+  Object.prototype.hasOwnProperty.call(results, action.type) ?
+    results[action.type] : isPasswordForgottenSent
 );
 export default forgotPassword;
