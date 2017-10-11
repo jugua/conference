3b26ab90a104732f673a49a9495ec48504a73918
Kateryna Buhaichuk <Kateryna_Buhaichuk@epam.com>
2017-10-11 18:01:51 +0300

    Add EndpointUrl.java

---
+++ qa/api/src/test/java/com/epam/cm/base/EndpointUrl.java
@@ -0,0 +1,17 @@
+package com.epam.cm.base;
+
+public class EndpointUrl {
+
+    public static final String LOGIN = "/api/login";
+    public static final String LOGOUT = "/api/logout/";
+    public static final String FORGOTPASS = "/api/forgot-password";
+    public static final String LANGUAGES = "/api/lang";
+    public static final String LEVELS = "/api/level";
+    public static final String USER_EXISTING = "api/user/1";
+    public static final String USER_NONEXISTING = "api/user/500";
+
+    public static final String PAST_CONFERENCE = "/api/conference/past";
+    public static final String UPCOMING_CONFERENCE = "/api/conference/upcoming";
+    public static final String UPDATE_CONFERENCE = "api/conference/update/9";
+    public static final String UPDATE_NON_EXISTING_CONFERENCE = "api/conference/update";
+}
