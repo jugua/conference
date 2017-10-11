edc86f47b846442304fd3dd63b23049a450f2aa4
Asan Ametov <Asan_Ametov@epam.com>
2017-10-11 13:59:33 +0300

    review issues fixed

--- conference/react-js/src/reducers/user.js
+++ conference/react-js/src/reducers/user.js
@@ -1,13 +1,7 @@
 import actionTypes from '../constants/actions-types';
 
-function user(state = {}, action) {
-  const { type, payload } = action;
-  switch (type) {
-  case actionTypes.SET_USER:
-    return payload;
-  default:
-    return state;
-  }
-}
+const user = (state = {}, { type, payload }) => (
+  type === actionTypes.SET_USER ? payload : state
+);
 
 export default user;
