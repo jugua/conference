5f6c5e515b8f7aa8b624ba45db1f47e0bfd27c30
Vadym Pyntia <vadym_pyntia@epam.com>
2017-10-11 16:19:44 +0300

    Conflict resolved

--- qa/api/src/test/java/com/epam/cm/base/EndPointURL.java
+++
@@ -1,6 +0,0 @@
-package com.epam.cm.base;
-
-public class EndPointURL {
-    public static final String API_USER_EXISTING = "api/user/1";
-    public static final String API_USER_NONEXISTING = "api/user/500";
-}
