28f37946cc64b6cb006508ebd276b153197213b2
Liena Bondarenko <Liena_Bondarenko@epam.com>
2017-10-12 17:18:41 +0300

    EndpointUrl.java

--- qa/api/src/test/java/com/epam/cm/base/EndpointUrl.java
+++ qa/api/src/test/java/com/epam/cm/base/EndpointUrl.java
@@ -6,4 +6,8 @@ public class EndpointUrl {
     public static final String TYPE = "/api/type";
     public static final String TOPIC = "/api/topic";
     public static final String CONFERENCE = "/api/conference";
+    public static final String LOGOUT = "/api/logout";
+    public static final String FORGOT_PASSWORD = "/api/forgot-password";
+    public static final String LANGUAGES = "/api/lang";
+    public static final String LEVELS = "/api/level";
 }
--- qa/api/src/test/java/com/epam/cm/base/EnvironmentUtils.java
+++ qa/api/src/test/java/com/epam/cm/base/EnvironmentUtils.java
@@ -20,4 +20,8 @@ public class EnvironmentUtils {
 
 
     }
+
+
+
 }
+
