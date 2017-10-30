f63c5522043a955478a65891f4ffd351312cc346
Mihail Melnik <mikhailimelnik@gmail.com>
2017-10-30 19:10:53 +0200

    add comment for empty method <eom>

--- conference/webapp/src/main/java/ua/rd/cm/web/controller/ForgotPasswordController.java
+++ conference/webapp/src/main/java/ua/rd/cm/web/controller/ForgotPasswordController.java
@@ -5,25 +5,30 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import lombok.AllArgsConstructor;
 import ua.rd.cm.domain.User;
 import ua.rd.cm.domain.VerificationToken;
 import ua.rd.cm.dto.MessageDto;
 import ua.rd.cm.dto.NewPasswordDto;
 import ua.rd.cm.infrastructure.mail.MailService;
+import ua.rd.cm.infrastructure.mail.preparator.ForgotMessagePreparator;
 import ua.rd.cm.services.businesslogic.UserService;
 import ua.rd.cm.services.businesslogic.impl.VerificationTokenService;
-import ua.rd.cm.infrastructure.mail.preparator.ForgotMessagePreparator;
 
 @RestController
 @AllArgsConstructor(onConstructor = @__({@Autowired}))
@@ -49,7 +54,8 @@ public class ForgotPasswordController {
                 responseMessage.setError("email_not_found");
             } else {
                 User currentUser = userService.getByEmail(node.get("mail").textValue());
-                VerificationToken token = tokenService.createToken(currentUser, VerificationToken.TokenType.FORGOT_PASS);
+                VerificationToken token = tokenService.createToken(currentUser, VerificationToken.TokenType
+                        .FORGOT_PASS);
                 tokenService.setPreviousTokensExpired(token);
                 tokenService.saveToken(token);
                 mailService.sendEmail(currentUser, new ForgotMessagePreparator(token, mailService.getUrl()));
@@ -66,12 +72,15 @@ public class ForgotPasswordController {
 
     @GetMapping("/changePassword/{token}")
     public ResponseEntity changePassword(@PathVariable String token) {
-        return withTokenGetRequestProcessor.process(token, VerificationToken.TokenType.FORGOT_PASS, verificationToken -> {
-        });
+        return withTokenGetRequestProcessor.process(
+                token, VerificationToken.TokenType.FORGOT_PASS, verificationToken -> {
+                    //NOP
+                });
     }
 
     @PostMapping("/changePassword/{token}")
-    public ResponseEntity changePassword(@PathVariable String token, @Valid @RequestBody NewPasswordDto dto, BindingResult bindingResult) {
+    public ResponseEntity changePassword(@PathVariable String token, @Valid @RequestBody NewPasswordDto dto,
+                                         BindingResult bindingResult) {
         VerificationToken verificationToken = tokenService.getToken(token);
         if (!isPasswordConfirmed(dto))
             return ResponseEntity.badRequest().build();
