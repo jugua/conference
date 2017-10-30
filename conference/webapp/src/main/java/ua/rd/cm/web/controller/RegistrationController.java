ec272f128f0143b955238b9e1998eca9dcb474c7
Mihail Melnik <mikhailimelnik@gmail.com>
2017-10-30 19:18:27 +0200

    reformat code in RegistrationController <eom>

--- conference/webapp/src/main/java/ua/rd/cm/web/controller/RegistrationController.java
+++ conference/webapp/src/main/java/ua/rd/cm/web/controller/RegistrationController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import ua.rd.cm.domain.Role;
 import ua.rd.cm.domain.User;
@@ -23,35 +25,29 @@ import ua.rd.cm.services.exception.EmailAlreadyExistsException;
 import ua.rd.cm.services.exception.PasswordMismatchException;
 
 
-@AllArgsConstructor(onConstructor = @__({@Autowired}))
+@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
 @RestController
 @RequestMapping("/registration")
 @Log4j
 public class RegistrationController {
 
+    private static final String VALIDATION_IS_FAILED = "Request for [registration] is failed: validation is failed. " +
+            "[HttpServletRequest:";
 
-    private final UserService userService;
+    @NonNull
+    private UserService userService;
 
     @PostMapping
     public ResponseEntity register(@Valid @RequestBody RegistrationDto dto,
                                    BindingResult bindingResult,
-                                   HttpServletRequest request
-    ) {
+                                   HttpServletRequest request) {
         dto.setUserStatus(User.UserStatus.UNCONFIRMED);
         dto.setRoleName(Role.SPEAKER);
         return processUserRegistration(dto, bindingResult, request);
     }
 
-    public void registration() {
-
-    }
-
-    public void confirmationProcess() {
-
-    }
-
-
-    private ResponseEntity processUserRegistration(RegistrationDto dto, BindingResult bindingResult, HttpServletRequest request) {
+    private ResponseEntity processUserRegistration(RegistrationDto dto, BindingResult bindingResult,
+                                                   HttpServletRequest request) {
         HttpStatus status;
         MessageDto message = new MessageDto();
 
@@ -60,7 +56,7 @@ public class RegistrationController {
             if (bindingResult.hasFieldErrors()) {
                 status = HttpStatus.BAD_REQUEST;
                 message.setError("empty_fields");
-                log.error("Request for [registration] is failed: validation is failed. [HttpServletRequest: " + request.toString() + "]");
+                log.error(VALIDATION_IS_FAILED + " " + request.toString() + "]");
             } else {
                 userService.checkUserRegistration(dto);
                 userService.registerNewUser(dto);
@@ -70,12 +66,13 @@ public class RegistrationController {
         } catch (PasswordMismatchException ex) {
             status = HttpStatus.BAD_REQUEST;
             message.setError(ex.getMessage());
-            log.error("Request for [registration] is failed: validation is failed. [HttpServletRequest: " + request.toString() + "]");
+            log.error(VALIDATION_IS_FAILED + " " + request.toString() + "]");
         } catch (EmailAlreadyExistsException ex) {
             status = HttpStatus.CONFLICT;
             message.setError(ex.getMessage());
             log.error("Registration failed: " + dto.toString() +
-                    ". Email '" + dto.getEmail() + "' is already in use. [HttpServletRequest: " + request.toString() + "]");
+                    ". Email '" + dto.getEmail() + "' is already in use. [HttpServletRequest: "
+                    + request.toString() + "]");
         }
 
         return ResponseEntity.status(status).body(message);
