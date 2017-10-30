0699de78f07fdf0a6d2368b6213b85cc8a513d32
Anastasiia Milinchuk <Anastasiia_Milinchuk@epam.com>
2017-10-30 19:55:10 +0200

    [controllers-refactoring] move changePassword method to SignInController
    Move changePassword from ForgotPasswordController to SignInController

--- conference/webapp/src/main/java/ua/rd/cm/web/controller/ForgotPasswordController.java
+++ conference/webapp/src/main/java/ua/rd/cm/web/controller/ForgotPasswordController.java
@@ -3,15 +3,10 @@ package ua.rd.cm.web.controller;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +19,6 @@ import lombok.AllArgsConstructor;
 import ua.rd.cm.domain.User;
 import ua.rd.cm.domain.VerificationToken;
 import ua.rd.cm.dto.MessageDto;
-import ua.rd.cm.dto.NewPasswordDto;
 import ua.rd.cm.infrastructure.mail.MailService;
 import ua.rd.cm.infrastructure.mail.preparator.ForgotMessagePreparator;
 import ua.rd.cm.services.businesslogic.UserService;
@@ -35,12 +29,10 @@ import ua.rd.cm.services.businesslogic.impl.VerificationTokenService;
 @RequestMapping("/forgotPasswordPage")
 public class ForgotPasswordController {
 
-    private final WithTokenGetRequestProcessor withTokenGetRequestProcessor;
-    private final VerificationTokenService tokenService;
-    private final UserService userService;
-    private final MailService mailService;
-    private final ObjectMapper objectMapper;
-    private final PasswordEncoder passwordEncoder;
+    private VerificationTokenService tokenService;
+    private UserService userService;
+    private MailService mailService;
+    private ObjectMapper objectMapper;
 
     @PostMapping("/forgotPassword")
     public ResponseEntity forgotPassword(@RequestBody String mail, HttpServletRequest request) throws IOException {
@@ -69,31 +61,4 @@ public class ForgotPasswordController {
 
         return ResponseEntity.status(httpStatus).body(responseMessage);
     }
-
-    @GetMapping("/changePassword/{token}")
-    public ResponseEntity changePassword(@PathVariable String token) {
-        return withTokenGetRequestProcessor.process(
-                token, VerificationToken.TokenType.FORGOT_PASS, verificationToken -> {
-                    //NOP
-                });
-    }
-
-    @PostMapping("/changePassword/{token}")
-    public ResponseEntity changePassword(@PathVariable String token, @Valid @RequestBody NewPasswordDto dto,
-                                         BindingResult bindingResult) {
-        VerificationToken verificationToken = tokenService.getToken(token);
-        if (!isPasswordConfirmed(dto))
-            return ResponseEntity.badRequest().build();
-
-        User currentUser = verificationToken.getUser();
-        currentUser.setPassword(passwordEncoder.encode(dto.getPassword()));
-        userService.updateUserProfile(currentUser);
-
-        return ResponseEntity.ok().build();
-    }
-
-    private boolean isPasswordConfirmed(NewPasswordDto dto) {
-        return dto.getPassword().equals(dto.getConfirm());
-    }
-
 }
--- conference/webapp/src/main/java/ua/rd/cm/web/controller/SignInController.java
+++ conference/webapp/src/main/java/ua/rd/cm/web/controller/SignInController.java
@@ -1,15 +1,61 @@
 package ua.rd.cm.web.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.rd.cm.domain.User;
+import ua.rd.cm.domain.VerificationToken;
+import ua.rd.cm.dto.NewPasswordDto;
+import ua.rd.cm.services.businesslogic.UserService;
+import ua.rd.cm.services.businesslogic.impl.VerificationTokenService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping
+@AllArgsConstructor(onConstructor = @__({@Autowired}))
 public class SignInController {
+
+    private WithTokenGetRequestProcessor withTokenGetRequestProcessor;
+    private PasswordEncoder passwordEncoder;
+    private VerificationTokenService tokenService;
+    private UserService userService;
 
     @PostMapping("/login")
     public void signIn() {
     }
 
+    @GetMapping("/changePassword/{token}")
+    public ResponseEntity changePassword(@PathVariable String token) {
+        return withTokenGetRequestProcessor.process(
+                token, VerificationToken.TokenType.FORGOT_PASS, verificationToken -> {
+                    //NOP
+                });
+    }
+
+    @PostMapping("/changePassword/{token}")
+    public ResponseEntity changePassword(@PathVariable String token, @Valid @RequestBody NewPasswordDto dto,
+                                         BindingResult bindingResult) {
+        VerificationToken verificationToken = tokenService.getToken(token);
+        if (!isPasswordConfirmed(dto))
+            return ResponseEntity.badRequest().build();
+
+        User currentUser = verificationToken.getUser();
+        currentUser.setPassword(passwordEncoder.encode(dto.getPassword()));
+        userService.updateUserProfile(currentUser);
+
+        return ResponseEntity.ok().build();
+    }
+
+    private boolean isPasswordConfirmed(NewPasswordDto dto) {
+        return dto.getPassword().equals(dto.getConfirm());
+    }
 }
