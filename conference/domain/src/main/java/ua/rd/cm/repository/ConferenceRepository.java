ed52637180fa544c11debcd64c6d262aeacd16bb
bogdanolexandr <bogdanolexandr@gmail.com>
2017-10-31 14:03:41 +0200

    add countByOrganiserId method to ConferenceRepository

--- conference/domain/src/main/java/ua/rd/cm/repository/ConferenceRepository.java
+++ conference/domain/src/main/java/ua/rd/cm/repository/ConferenceRepository.java
@@ -3,7 +3,9 @@ package ua.rd.cm.repository;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import ua.rd.cm.domain.Conference;
@@ -19,4 +21,7 @@ public interface ConferenceRepository extends CrudRepository<Conference, Long> {
     List<Conference> findAll();
 
     Conference findById(Long id);
+    
+    @Query("SELECT count(c) FROM Conference c JOIN c.organisers o WHERE o.id = :userId")
+    Long countByOrganiserId(@Param("userId")Long id);
 }
