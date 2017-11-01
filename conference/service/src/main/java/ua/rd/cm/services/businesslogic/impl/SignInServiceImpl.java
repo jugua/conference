e33a6cb74b3430097d26b5b94a4fae332b3ec82c
bogdanolexandr <bogdanolexandr@gmail.com>
2017-11-01 16:15:55 +0200

    update signInServiceImpl

--- conference/service/src/main/java/ua/rd/cm/services/businesslogic/impl/SignInServiceImpl.java
+++ conference/service/src/main/java/ua/rd/cm/services/businesslogic/impl/SignInServiceImpl.java
@@ -1,13 +1,10 @@
 package ua.rd.cm.services.businesslogic.impl;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ua.rd.cm.domain.User;
 import ua.rd.cm.dto.LoginDto;
-import ua.rd.cm.repository.ConferenceRepository;
 import ua.rd.cm.repository.TalkRepository;
 import ua.rd.cm.repository.UserRepository;
 import ua.rd.cm.services.businesslogic.SignInService;
@@ -24,10 +21,10 @@ public class SignInServiceImpl implements SignInService {
 	public LoginDto login(String email, String password) {
 		User user = userRepository.findByEmail(email);
 		String firstName = user.getFirstName();
-		List<String> roles = user.getRoleNames();
+		String role = user.getRoleNames().get(0);
 		long conferenceCount = user.getOrganizerConferences().size();
 		long talksCount = talkRepository.countByUserId(user.getId());
-		LoginDto loginDto = new LoginDto(firstName, roles, conferenceCount, talksCount);
+		LoginDto loginDto = new LoginDto(firstName, role, conferenceCount, talksCount);
 		return loginDto;
 	}
 
