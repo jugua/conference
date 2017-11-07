446962efc9c8c75c243fe9701fc2f1bbb02f18a3
milinchuk <anastasia.milinchuk@gmail.com>
2017-11-08 00:04:08 +0200

    add all args constructor to VerificationToken, add @RequiredArgsConstructor and @Builder annotations <eom>

--- conference/domain/src/main/java/ua/rd/cm/domain/VerificationToken.java
+++ conference/domain/src/main/java/ua/rd/cm/domain/VerificationToken.java
@@ -10,36 +10,56 @@ import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 @Entity
 public class VerificationToken extends AbstractEntity {
+
     public static final int EXPIRATION_IN_MINUTES = 60;
 
+    @NonNull
     @Column(nullable = false)
     private String token;
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @NonNull
     @Column(name = "expiry_date", nullable = false)
     private LocalDateTime expiryDate;
 
+    @NonNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private TokenType type;
 
+    @NonNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private TokenStatus status;
+
+    @Builder
+    public VerificationToken(Long id, String token,
+                             User user, LocalDateTime expiryDate,
+                             TokenType type, TokenStatus status) {
+        super(id);
+        this.token = token;
+        this.user = user;
+        this.expiryDate = expiryDate;
+        this.type = type;
+        this.status = status;
+    }
 
     public long calculateSecondsToExpiry() {
         return ChronoUnit.SECONDS.between(LocalDateTime.now(), expiryDate);
