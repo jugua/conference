d6c8b48d292ccd82918486b2b79153ba2aad97fb
Mihail Melnik <mikhailimelnik@gmail.com>
2017-11-13 01:37:16 +0200

    introduce default (xxx_xxx) column name generation strategy in the ConferencePhysicalNamingStrategy <eom>

--- conference/domain/src/main/java/domain/config/ConferencePhysicalNamingStrategy.java
+++ conference/domain/src/main/java/domain/config/ConferencePhysicalNamingStrategy.java
@@ -36,7 +36,8 @@ public class ConferencePhysicalNamingStrategy implements PhysicalNamingStrategy 
 
     @Override
     public Identifier toPhysicalColumnName(Identifier name, JdbcEnvironment jdbcEnvironment) {
-        return name;
+        final List<String> parts = splitAndReplace(name.getText());
+        return jdbcEnvironment.getIdentifierHelper().toIdentifier(join(parts), name.isQuoted());
     }
 
     private LinkedList<String> splitAndReplace(String name) {
