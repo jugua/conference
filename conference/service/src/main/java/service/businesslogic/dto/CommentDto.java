4ed567a9f0321e9481765afde73c3d8bb4caf817
bogdanolexandr <bogdanolexandr@gmail.com>
2017-11-17 15:47:01 +0200

    create CommentDto

---
+++ conference/service/src/main/java/service/businesslogic/dto/CommentDto.java
@@ -0,0 +1,33 @@
+package service.businesslogic.dto;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDto {
+
+	@JsonProperty("id")
+	@NonNull
+	private Long id;
+	@JsonProperty("message")
+	@NonNull
+	private String message;
+	@JsonProperty("userId")
+	@NonNull
+	private Long userId;
+	@JsonProperty("talkId")
+	@NonNull
+	private Long talkId;
+	@JsonProperty("time")
+	@NonNull
+	private LocalDateTime time;
+	
+}
