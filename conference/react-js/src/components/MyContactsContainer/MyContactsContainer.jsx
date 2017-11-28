2dfa13e95338b6658f2c3c06533a76173a41f588
Oleksandr Kutkanych <Oleksandr_Kutkanych@epam.com>
2017-11-28 11:07:47 +0200

    first render

--- conference/react-js/src/components/MyContactsContainer/MyContactsContainer.jsx
+++ conference/react-js/src/components/MyContactsContainer/MyContactsContainer.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
-// import InputBlock from '../InputBlock/InputBlock';
+import InputBlock from '../InputBlock/InputBlock';
 
 class MyContactsContainer extends Component {
   constructor(props) {
@@ -18,49 +18,64 @@ class MyContactsContainer extends Component {
     axios.get(contactsURL)
       .then((data) => {
         this.setState({ arr: data.data });
-        console.log(data.data);
       });
   };
 
-  handleInput = ({ target: { value } }, i) => {
-    const z = this.state.arr[i];
-    console.log(z);
-    console.log({ arr: [...this.state.arr, ...this.state.arr[i], { id: i, value }] });
-  // const items = this.state.userContact;
+  // handleInput = ({ target: { value } }, i) => {
+  //   const z = this.state.arr[i];
+  // const items = this.state.arr;
   // items[item].value = e.target.value;
-  // update state
-  // this.setState({ userContact: items });
-  // this.state(prevState => ({...prevState.userContact[i], value:value }))
+  //   console.log(z);
+  //   console.log({ arr: [...this.state.arr, ...this.state.arr[i], { id: i, value }] });
+  // // const items = this.state.userContact;
+  // // items[item].value = e.target.value;
+  // // update state
+  // // this.setState({ userContact: items });
+  // // this.state(prevState => ({...prevState.userContact[i], value:value }))
+  //
+  // // this.setState((prevState) => {
+  // //   const current = prevState.userContact[i].value;
+  // //   return { [current]: value };
+  // // });
+  //
+  // // onChange={e => (this.handleInput(e, i))}
+  // };
 
-  // this.setState((prevState) => {
-  //   const current = prevState.userContact[i].value;
-  //   return { [current]: value };
-  // });
-
-  // onChange={e => (this.handleInput(e, i))}
+  handleInput = (e, i) => {
+    const items = this.state.arr;
+    items[i - 1].value = e.target.value;
+    this.setState({
+      arr: [...this.state.arr, ...this.state.arr[i - 1], { value: e.target.value }],
+    });
   };
 
   renderElement = arr => arr.map(({ id, value }) => (
-    <div key={id} >
-      <span>{id}</span>
-      <input
-        value={value}
-        onChange={e => (this.handleInput(e, id))}
-      />
-    </div>))
+    <InputBlock
+      key={id}
+      label="name"
+      labelClass="form-label"
+      inputClass="sign-up__field"
+      value={value}
+      name="value"
+      onChange={(e) => { this.handleInput(e, id); }}
+    />
+  ));
 
   render() {
     return (
       <div>
-        <h1>test</h1>
-        {this.renderElement(this.state.userContacts)}
+        <form
+          className="sign-up"
+        >
+          {this.renderElement(this.state.arr)}
+        </form>
       </div>
     );
   }
 }
 
 MyContactsContainer.propTypes = {
-  user: PropTypes.objectOf(PropTypes.number).isRequired,
+  user: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export default MyContactsContainer;
