225d6c90c9405bc68b5231a9b27999ca3f607590
bogdanolexandr <bogdanolexandr@gmail.com>
2017-12-06 13:54:40 +0200

    update TopicDto

--- conference/service/src/main/java/service/businesslogic/dto/TopicDto.java
+++ conference/service/src/main/java/service/businesslogic/dto/TopicDto.java
@@ -1,11 +1,10 @@
 package service.businesslogic.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Data;
 
 @Data
 public class TopicDto {
-	@JsonProperty("name")
+	
 	private String name;
+	
 }
