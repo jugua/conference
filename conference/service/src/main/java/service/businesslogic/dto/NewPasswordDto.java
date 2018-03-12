8ba8bdbdb564b8e0e041583f217af8ab92437ff8
Mihail Melnik <mikhailimelnik@gmail.com>
2018-03-12 23:34:36 +0200

    code: add missing neccessary Lombok annotations
    - add @NoArgsConstructor/@RequiredArgsConstructor annotations
      for several classes as they're needed by other classes

--- conference/service/src/main/java/service/businesslogic/dto/NewPasswordDto.java
+++ conference/service/src/main/java/service/businesslogic/dto/NewPasswordDto.java
@@ -6,9 +6,13 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class NewPasswordDto {
 
     @JsonProperty("newPassword")
