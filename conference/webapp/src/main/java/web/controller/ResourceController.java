d3d758ec95238661d185f6abc9123069e5291ed3
Mihail Melnik <mikhailimelnik@gmail.com>
2018-05-05 00:54:12 +0300

    refactoring: format code, rename fields <eom>

--- conference/webapp/src/main/java/web/controller/ResourceController.java
+++ conference/webapp/src/main/java/web/controller/ResourceController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import domain.model.Language;
 import lombok.AllArgsConstructor;
+
+import domain.model.Language;
 import service.businesslogic.api.LanguageService;
 import service.businesslogic.api.LevelService;
 import service.businesslogic.api.TopicService;
@@ -22,35 +23,36 @@ import service.businesslogic.dto.TypeDto;
 
 @RestController
 @RequestMapping("/")
-@AllArgsConstructor(onConstructor = @__({ @Autowired }))
+@AllArgsConstructor(onConstructor = @__({@Autowired}))
 public class ResourceController {
-	private final TypeService typeService;
-	private final TopicService topicService;
-	private final LevelService levelService;
-	private final LanguageService languageService;
 
-	@PreAuthorize("isAuthenticated()")
-	@GetMapping("types")
-	public ResponseEntity<List<TypeDto>> getTypes() {
-		return new ResponseEntity<>(typeService.findAll(), HttpStatus.OK);
-	}
+    private final TypeService types;
+    private final TopicService topics;
+    private final LevelService levels;
+    private final LanguageService languages;
 
-	@PreAuthorize("isAuthenticated()")
-	@GetMapping("topics")
-	public ResponseEntity<List<TopicDto>> getTopics() {
-		return new ResponseEntity<>(topicService.findAll(), HttpStatus.OK);
-	}
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("types")
+    public ResponseEntity<List<TypeDto>> getTypes() {
+        return new ResponseEntity<>(types.findAll(), HttpStatus.OK);
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@GetMapping("levels")
-	public ResponseEntity<List<LevelDto>> getLevels() {
-		return new ResponseEntity<>(levelService.findAll(), HttpStatus.OK);
-	}
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("topics")
+    public ResponseEntity<List<TopicDto>> getTopics() {
+        return new ResponseEntity<>(topics.findAll(), HttpStatus.OK);
+    }
 
-	@PreAuthorize("isAuthenticated()")
-	@GetMapping("languages")
-	public ResponseEntity<List<Language>> getLanguages() {
-		return new ResponseEntity<>(languageService.findAll(), HttpStatus.OK);
-	}
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("levels")
+    public ResponseEntity<List<LevelDto>> getLevels() {
+        return new ResponseEntity<>(levels.findAll(), HttpStatus.OK);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("languages")
+    public ResponseEntity<List<Language>> getLanguages() {
+        return new ResponseEntity<>(languages.findAll(), HttpStatus.OK);
+    }
 
 }
